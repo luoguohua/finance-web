6a07bc028a9e5a273c9694795e21f7190c244000
luoguohua <luoguohua@seerbigdata.com>
2022-06-15 11:14:08 +0800

    init

---
+++ src/layout/components/Settings/index.vue
@@ -0,0 +1,143 @@
+<template>
+  <el-drawer
+    title="系统布局配置"
+    :visible.sync="flag"
+    :close-on-press-escape="false"
+    :wrapper-closable="true"
+    :size="300"
+    :before-close="handleClose"
+    direction="rtl"
+  >
+    <div class="drawer-container">
+      <div>
+        <div class="drawer-item">
+          <span>{{ $t('settings.theme') }}</span>
+          <theme-picker style="float: right;height: 26px;margin: -3px 8px 0 0;" @change="themeChange" />
+        </div>
+        <div class="drawer-item">
+          <span>侧栏风格</span>
+          <el-switch
+            v-model="sidebarColor"
+            class="drawer-switch"
+            active-value="dark"
+            inactive-value="white"
+            active-text="黑色"
+            inactive-text="白色"
+          />
+        </div>
+
+        <div class="drawer-item">
+          <span>{{ $t('settings.tagsView') }}</span>
+          <el-switch
+            v-model="tagsView"
+            class="drawer-switch"
+            active-text="开启"
+            inactive-text="关闭"
+          />
+        </div>
+
+        <div class="drawer-item">
+          <span>{{ $t('settings.fixedHeader') }}</span>
+          <el-switch
+            v-model="fixedHeader"
+            class="drawer-switch"
+            active-text="固定"
+            inactive-text="随动"
+          />
+        </div>
+
+        <div class="drawer-item">
+          <span>{{ $t('settings.sidebarLogo') }}</span>
+          <el-switch
+            v-model="sidebarLogo"
+            class="drawer-switch"
+            active-text="开启"
+            inactive-text="关闭"
+          />
+        </div>
+      </div>
+    </div>
+  </el-drawer>
+</template>
+
+<script>
+import ThemePicker from '@/components/ThemePicker'
+
+export default {
+  components: { ThemePicker },
+  data() {
+    return {
+      flag: this.$store.state.setting.settingBar.opened
+    }
+  },
+  computed: {
+    sidebarColor: {
+      get() {
+        return this.$store.state.setting.sideBarTheme
+      },
+      set(val) {
+        this.$store.commit('setting/setSideBarTheme', val)
+        this.$put('system/user/theme', { theme: val })
+      }
+    },
+    fixedHeader: {
+      get() {
+        return this.$store.state.setting.fixHeader
+      },
+      set(val) {
+        this.$store.commit('setting/fixHeader', val)
+      }
+    },
+    tagsView: {
+      get() {
+        return this.$store.state.setting.multipage
+      },
+      set(val) {
+        this.$store.commit('setting/setMultipage', val)
+      }
+    },
+    sidebarLogo: {
+      get() {
+        return this.$store.state.setting.sidebarLogo
+      },
+      set(val) {
+        this.$store.commit('setting/setSidebarLogo', val)
+      }
+    }
+  },
+  methods: {
+    themeChange(val) {
+      this.$store.commit('setting/setTheme', val)
+    },
+    handleClose() {
+      this.$store.commit('setting/openSettingBar', false)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.drawer-container {
+  padding: 24px;
+  font-size: 14px;
+  line-height: 1.5;
+  word-wrap: break-word;
+
+  .drawer-title {
+    margin-bottom: 12px;
+    color: rgba(0, 0, 0, .85);
+    font-size: 14px;
+    line-height: 22px;
+  }
+
+  .drawer-item {
+    color: rgba(0, 0, 0, .65);
+    font-size: 14px;
+    padding: 12px 0;
+  }
+
+  .drawer-switch {
+    float: right
+  }
+}
+</style>
---
+++ src/views/profile/components/Timeline.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="block" style="padding-top: 10px">
+    <template v-if="finished">
+      <el-timeline>
+        <el-timeline-item v-for="(item,index) of timeline" :key="index" :timestamp="item.loginTime" placement="top">
+          <el-card>
+            <p><el-icon class="el-icon-link" />  {{ $t('table.loginLog.ip') }}：{{ item.ip }} </p>
+            <p><el-icon class="el-icon-bangzhu" />  {{ $t('table.loginLog.browser') }}：{{ item.browser }} </p>
+            <p><el-icon class="el-icon-monitor" />  {{ $t('table.loginLog.system') }}：{{ item.system }} </p>
+          </el-card>
+        </el-timeline-item>
+      </el-timeline>
+    </template>
+    <template v-else>
+      <el-skeleton :rows="20" animated />
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      timeline: [],
+      finished: false
+    }
+  },
+  mounted() {
+    this.getTimeLine()
+  },
+  methods: {
+    getTimeLine() {
+      this.finished = false
+      this.$get('system/loginLog/currentUser').then((r) => {
+        this.timeline = r.data.data
+        this.finished = true
+      })
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.el-card.is-always-shadow {
+  box-shadow: none;
+}
+.el-card {
+  border: 1px solid #f1f1f1;
+  border-radius: 2px;
+}
+</style>
